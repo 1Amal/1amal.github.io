66e775625a9eaa6410a631a83981c045f4f0a568
Amal K <12756149+1Amal@users.noreply.github.com>
2022-08-16 21:23:46 +1000

    Added JavaScript, CSS changes
    Added JavaScript, CSS changes to Navigation bar.
    Added a Footer section

---
+++ mainJs.js
@@ -0,0 +1,18 @@
+//Amal Kariyawasam, 2022
+
+/* This is
+    the main
+        JavaScript file for 
+            this portfolio website */
+
+// Following function will show time at page load
+function time_function()
+{
+    document.getElementById("time").innerHTML=Date();
+
+}
+
+function page_version()
+{
+    document.getElementById("version").innerHTML="Last Updated on 16/08/2022"
+}
