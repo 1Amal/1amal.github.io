5a443cdb555efac8c845378b4dafaead53cf03cc
Amal <12756149+1Amal@users.noreply.github.com>
2022-10-30 11:49:33 +1100

    Fix missing DIV and fix code indentation

--- mainJs.js
+++ mainJs.js
@@ -14,7 +14,7 @@ function time_function()
 
 function page_version()
 {
-    document.getElementById("version").innerHTML="Content last updated on 16/10/2022"
+    document.getElementById("version").innerHTML="Content last updated on 30/10/2022"
 }
 
 time_function()
