599f03db4ef7c87ded9a2d9b3c5b72a49609c936
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-08-23 15:20:46 +1000

    Added SVG Logos to the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode:'development',
-  entry: './src/mainJavaScript.js',
+  mode: "development",
+  entry: "./src/mainJavaScript.js",
   output: {
-    filename: 'mainJavaScript.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "mainJavaScript.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  
+
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your original index.html file
+      template: "./src/index.html", // Path to your original index.html file
     }),
   ],
 };
