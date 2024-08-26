586c74fd6b5b07795d19ac15ca0955bbc8bc474a
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-08-26 12:28:16 +1000

    Renamed resources and organized into folder Re-organized the images, style sheet locations, etc.. into relevant folders and renamed files into lower case and separated names with hyphens so web servers can host the files without problems

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/mainJavaScript.js",
+  entry: "./src/website-resources/main-javascript.js",
   output: {
-    filename: "mainJavaScript.js",
+    filename: "main-javascript.js",
     path: path.resolve(__dirname, "dist"),
   },
 
