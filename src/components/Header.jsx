670df7d53d61c45820fac72074be025527f6f6ed
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-02-25 12:04:02 +1100

    Added Backend Stack logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 function Header() {
   return (
     <div>
-         <a href="https://github.com/amaldevelops" target="_blank"><img src="/logos/logo-github.svg" className="externalLinkLogos" alt="github Logo"/></a> <br></br>
-      <a href="https://www.linkedin.com/in/amaldevelops/" target="blank"><img src="/logos/logo-linkedin.svg" alt="Linkedin Logo" className="externalLinkLogos"/></a>
+      <a href="https://github.com/amaldevelops" target="_blank">
+        <img
+          src="/logos/logo-github.svg"
+          className="externalLinkLogos"
+          alt="github Logo"
+        />
+      </a>{" "}
+      <br></br>
+      <a href="https://www.linkedin.com/in/amaldevelops/" target="blank">
+        <img
+          src="/logos/logo-linkedin.svg"
+          alt="Linkedin Logo"
+          className="externalLinkLogos"
+        />
+      </a>
     </div>
   );
 }
--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -1,20 +1,65 @@
-function TechStack()
-{
-    return(
-        <div>
-            <h1>Tech Stack</h1>
-            <div>
-            <h2>Front End</h2>
-            <img src="/logos/logo-js.svg"  className="techStackLogos" alt="JS Logo"></img>
+function TechStack() {
+  return (
+    <div>
+      <h1>Tech Stack</h1>
+      <div>
+        <h2>Front End</h2>
+        <img
+          src="/logos/logo-js.svg"
+          className="techStackLogos"
+          alt="JS Logo"
+        ></img>
 
-            <img src="/logos/logo-html.svg"  className="techStackLogos" alt="HTML Logo"></img>
-            <img src="/logos/logo-css.svg"  className="techStackLogos" alt="CSS Logo"></img>
+        <img
+          src="/logos/logo-react.svg"
+          className="techStackLogos"
+          alt="React Logo"
+        />
 
+        <img
+          src="/logos/logo-html.svg"
+          className="techStackLogos"
+          alt="HTML Logo"
+        ></img>
+        <img
+          src="/logos/logo-css.svg"
+          className="techStackLogos"
+          alt="CSS Logo"
+        ></img>
+      </div>
 
-            </div>
-            
-        </div>
-    )
+      <div>
+        <h2>Back End</h2>
+        <img
+          src="/logos/logo-nodejs.svg"
+          className="techStackLogos"
+          alt="NodeJs Logo"
+        ></img>
+
+        <img
+          src="/logos/logo-express.svg"
+          className="techStackLogos"
+          alt="Express Logo"
+        ></img>
+
+        <img
+          src="/logos/logo-postgressql.svg"
+          className="techStackLogos"
+          alt="PostgreSQL Logo"
+        ></img>
+
+        <img
+          src="/logos/logo-prisma.svg"
+          className="techStackLogos"
+          alt="Prisma Logo"
+        ></img>
+      </div>
+
+      <div>
+        <h2>Tools</h2>
+      </div>
+    </div>
+  );
 }
 
 export default TechStack;
